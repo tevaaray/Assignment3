bb6a16f6804e67db0416c22d5ea080073517e2ce
tevaaray <241313@astanait.edu.kz>
2025-04-24 15:52:58 +0500

    MyHashTable

--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -4,6 +4,8 @@ public class MyHashTable<K, V> {
         private V value;
         private HashNode<K, V> next;
 
+
+
         public HashNode(K key, V value) {
             this.key = key;
             this.value = value;
