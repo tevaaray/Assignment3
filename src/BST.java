0fb729060c6fc211203c0fdd4c0cf3a161dc48e5
tevaaray <241313@astanait.edu.kz>
2025-04-27 12:52:31 +0500

    BST

---
+++ src/BST.java
@@ -0,0 +1,135 @@
+import java.util.*;
+public class BST<K extends Comparable<K>, V> implements Iterable<BST<K, V>.Entry> {
+    private Node root;
+    private int size = 0;
+    private class Node {
+        private K key;
+        private V val;
+        private Node left, right;
+
+        public Node(K key, V val) {
+            this.key = key;
+            this.val = val;
+        }
+    }
+    public class Entry {
+        private K key;
+        private V value;
+
+        public Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+    }
+
+    public void put(K key, V val) {
+        root = put(root, key, val);
+    }
+
+    private Node put(Node node, K key, V val) {
+        if (node == null) {
+            size++;
+            return new Node(key, val);
+        }
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0)
+            node.left = put(node.left, key, val);
+        else if (cmp > 0)
+            node.right = put(node.right, key, val);
+        else
+            node.val = val;
+        return node;
+    }
+
+    public V get(K key) {
+        Node node = get(root, key);
+        return node == null ? null : node.val;
+    }
+
+    private Node get(Node node, K key) {
+        if (node == null) return null;
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) return get(node.left, key);
+        else if (cmp > 0) return get(node.right, key);
+        else return node;
+    }
+
+    public void delete(K key) {
+        root = delete(root, key);
+    }
+
+    private Node delete(Node node, K key) {
+        if (node == null) return null;
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0)
+            node.left = delete(node.left, key);
+        else if (cmp > 0)
+            node.right = delete(node.right, key);
+        else {
+            size--;
+            if (node.right == null) return node.left;
+            if (node.left == null) return node.right;
+
+            Node t = node;
+            node = min(t.right);
+            node.right = deleteMin(t.right);
+            node.left = t.left;
+        }
+        return node;
+    }
+
+    private Node min(Node node) {
+        if (node.left == null) return node;
+        return min(node.left);
+    }
+
+    private Node deleteMin(Node node) {
+        if (node.left == null) return node.right;
+        node.left = deleteMin(node.left);
+        return node;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public Iterator<Entry> iterator() {
+        return new BSTIterator();
+    }
+
+    private class BSTIterator implements Iterator<Entry> {
+        private Stack<Node> stack = new Stack<>();
+
+        public BSTIterator() {
+            pushLeft(root);
+        }
+
+        private void pushLeft(Node node) {
+            while (node != null) {
+                stack.push(node);
+                node = node.left;
+            }
+        }
+
+        @Override
+        public boolean hasNext() {
+            return !stack.isEmpty();
+        }
+
+        @Override
+        public Entry next() {
+            Node current = stack.pop();
+            pushLeft(current.right);
+            return new Entry(current.key, current.val);
+        }
+    }
+}
